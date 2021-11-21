d536093289a1767e2f5a71635bffc5152b8100e6
aligator <aligator@suncraft-server.de>
2021-11-22 00:40:03 +0100

    Use common function for both walk implementations

--- walk.go
+++ walk.go
@@ -12,6 +12,39 @@ type ignoreFS struct {
 	*NoGo
 }
 
+func walkFN(ifs *ignoreFS, ignoreFileName string, path string, isDir bool, err error) (bool, error) {
+	if err != nil {
+		return false, err
+	}
+
+	if path != "." {
+		match := ifs.MatchPath(path)
+		if match.OnlyFolder && !isDir {
+			match.Matches = false
+		}
+
+		// If the rule is a negation rule, still proceed.
+		if match.Matches && !match.Rule.Negate {
+			if isDir {
+				return false, fs.SkipDir
+			}
+			return false, nil
+		}
+	}
+
+	if isDir {
+		// Add the ignore files when touching a new folder.
+		// That way we do not need to read all ignore files in advance.
+		// THis works because WalkDir runs in a deterministic way.
+		err := ifs.AddFile(filepath.Join(path, ignoreFileName))
+		if err != nil && !errors.Is(err, fs.ErrNotExist) {
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
 // AferoWalk walks the file tree rooted at root, calling walkFn for each file or
 // directory in the tree, including root. All errors that arise visiting files
 // and directories are filtered by walkFn. The files are walked in lexical
@@ -34,36 +67,16 @@ func AferoWalk(fsys afero.Fs, ignoreFileName string, fn filepath.WalkFunc, optio
 	}
 
 	return afero.Walk(fsys, ".", func(path string, info fs.FileInfo, err error) error {
+		ok, err := walkFN(ifs, ignoreFileName, path, info.IsDir(), err)
 		if err != nil {
 			return err
 		}
 
-		if path != "." {
-			match := ifs.MatchPath(path)
-			if match.OnlyFolder && !info.IsDir() {
-				match.Matches = false
-			}
-
-			// If the rule is a negation rule, still proceed.
-			if match.Matches && !match.Rule.Negate {
-				if info.IsDir() {
-					return fs.SkipDir
-				}
-				return nil
-			}
+		if ok {
+			return fn(path, info, err)
 		}
 
-		if info.IsDir() {
-			// Add the ignore files when touching a new folder.
-			// That way we do not need to read all ignore files in advance.
-			// THis works because WalkDir runs in a deterministic way.
-			err := ifs.AddFile(filepath.Join(path, ignoreFileName))
-			if err != nil && !errors.Is(err, fs.ErrNotExist) {
-				return err
-			}
-		}
-
-		return fn(path, info, err)
+		return nil
 	})
 }
 
@@ -93,35 +106,15 @@ func WalkDir(fsys fs.FS, ignoreFileName string, root string, fn fs.WalkDirFunc, 
 	}
 
 	return fs.WalkDir(ifs, root, func(path string, d fs.DirEntry, err error) error {
+		ok, err := walkFN(ifs, ignoreFileName, path, d.IsDir(), err)
 		if err != nil {
 			return err
 		}
 
-		if path != "." {
-			match := ifs.MatchPath(path)
-			if match.OnlyFolder && !d.IsDir() {
-				match.Matches = false
-			}
-
-			// If the rule is a negation rule, still proceed.
-			if match.Matches && !match.Rule.Negate {
-				if d.IsDir() {
-					return fs.SkipDir
-				}
-				return nil
-			}
+		if ok {
+			return fn(path, d, err)
 		}
 
-		if d.IsDir() {
-			// Add the ignore files when touching a new folder.
-			// That way we do not need to read all ignore files in advance.
-			// THis works because WalkDir runs in a deterministic way.
-			err := ifs.AddFile(filepath.Join(path, ignoreFileName))
-			if err != nil && !errors.Is(err, fs.ErrNotExist) {
-				return err
-			}
-		}
-
-		return fn(path, d, err)
+		return nil
 	})
 }
